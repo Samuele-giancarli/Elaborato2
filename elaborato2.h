cb62caa2b938463fc49844718f28b99c8e1f68cb
muni106 <mounir.samite@gmail.com>
2021-09-29 14:46:41 +0200

    NUM_OF_ROOTS

--- elaborato2.h
+++ elaborato2.h
@@ -1,7 +1,12 @@
+#include <math.h>
 /*
  * f(x) = ax^2 + bx + c
  * */
-#define NUM_OF_ROOTS(a,b,c) \
-{                           \
-                            \
+#define NUM_OF_ROOTS(double a, double b,double c)
+{
+    int radice;
+    double Delta = pow(b, 2)-(4*a*c);
+    (Delta < 0) ? radice=0 : ((Delta==0) ? radice = 1 : radice = 2);
+    return radice;
+
 }
