ff998e96f7fd7e75138dcb9fadf468d17b7513d1
muni106 <mounir.samite@gmail.com>
2021-09-29 14:56:38 +0200

    ci siamo

--- elaborato2.h
+++ elaborato2.h
@@ -2,11 +2,14 @@
 /*
  * f(x) = ax^2 + bx + c
  * */
-#define NUM_OF_ROOTS(double a, double b,double c)
-{
-    int radice;
-    double Delta = pow(b, 2)-(4*a*c);
-    (Delta < 0) ? radice=0 : ((Delta==0) ? radice = 1 : radice = 2);
-    return radice;
+#define NUM_OF_ROOTS(double a, double b,double c)\
+{\
+    int radice;\
+    double Delta = pow(b, 2)-(4*a*c);\
+    (Delta < 0) ? radice=0 : ((Delta==0) ? radice = 1 : radice = 2);\
+    return radice;\
+}
 
+#define ROOT1(double a, double b, double c){ \
+                                             \
 }
